125ba09cb9a77346ebff02f1674d7f0f38509b21
Jon Bassen <jbassen@stanford.edu>
2015-02-10 00:54:39 -0800

    times

--- getData.js
+++ getData.js
@@ -9,6 +9,7 @@ var User = require('./models/User');
 
 mongoose.connect(process.env.MONGOHQ_URL);
 var summary = {};
+var naList = {"jbassen": "", "dill": "", "mpkim": "", "kgibb": "", "nayakne": "", "kggriswo": ""};
 
 // data = [{exercise, username, timeMil, timeStr, hasAttempted, willAttempt, isPassing, willPass, currentSub, finalSub}]
 
@@ -16,16 +17,23 @@ Interaction
 .find()
 //.where("time").lt(onTimeDeadline)
 .where('exercise').gt(2).lt(12)
-.where("username").ne("jbassen").ne("dill")
+.where("username") //.ne("jbassen").ne("dill")
 .sort({ 'exercise': 1, 'username': 1, 'time': 1 })
 .exec(function(err, subs) {
 
   mongoose.connection.close();
 
   var attempts = 0;
+  var willPass = false;
 
   for(var i = 0; i < subs.length; i++) {
+    if( _.has(naList, subs[i].username) ) {
+      continue;
+    }
     attempts += 1;
+    if( _.has(JSON.parse(subs[i].grade), "message") ) {
+      willPass = true;
+    }
     var willAttempt = attempts;
     if (i < subs.length - 1) {
       if (subs[i].username === subs[i+1].username) {
@@ -43,22 +51,27 @@ Interaction
       summary[exercise] = {}
     }
 
+    //console.log( _.has(JSON.parse(subs[i].grade), "message") );
+    // /console.log(exercise + ": " + willPass);
+
     summary[exercise][username] = {
       willAttempt: willAttempt,
       fTimeMillis: timeMil,
       fTimeString: timeStr,
-      willPass: false//,
+      willPass: willPass //,
       //finalSub: JSON.parse(subs[i].answer).proof
     };
 
-    if( JSON.parse(subs[i].grade).message === "Success!" ) {
-      summary[exercise][username].willPass = true;
-    }
-
+    willPass = false;
   }
+
+  //console.log(summary);
 
   var data = [];
   for(var i = 0; i < subs.length; i++) {
+    if( _.has( naList, subs[i].username ) ) {
+      continue;
+    }
     attempts += 1;
     var hasAttempted = attempts;
     if (i < subs.length - 1) {
@@ -67,22 +80,23 @@ Interaction
       }
     }
 
+    var isPassing = false;
+    if( _.has(JSON.parse(subs[i].grade), "message") ) {
+      isPassing = true;
+    }
+
     data.push({
       exercise: subs[i].exercise.toString(),
       username: subs[i].username,
       timeMil: subs[i].time.getTime(),
       timeStr: subs[i].time.toString(),
       hasAttempted: hasAttempted,
-      willAttempt: summary[exercise][username].willAttempt,
-      isPassing: false,
-      willPass: summary[exercise][username].willPass//,
+      willAttempt: summary[subs[i].exercise][subs[i].username].willAttempt,
+      isPassing: isPassing,
+      willPass: summary[subs[i].exercise][subs[i].username].willPass //,
       //currentSub: JSON.parse(subs[i].answer).proof,
       //finalSub: summary[username][exercise].finalSub
     });
-
-    if( JSON.parse(subs[i].grade).message === "Success!" ) {
-      summary[exercise][username].isPassing = true;
-    }
 
   }
 
--- public/javascripts/times.js
+++ public/javascripts/times.js
@@ -1,13 +1,27 @@
-var _id = 9;
+var _id = "3";
 var ds = [];
 
 //Width and height
-var w = 1600;
-var h = 1200;
+var w = 1250;
+var h = 750;
+var p = 25; //padding
+
+var earlyDeadline;
+var onTimeDeadline;
+if ( parseInt(_id) < 7 ) {
+  earlyDeadline = new Date(2015, 0, 14, 00, 05, 0).getTime();
+  onTimeDeadline = new Date(2015, 0, 16, 12, 35, 0).getTime();
+} else if ( parseInt(_id) < 12) {
+  earlyDeadline = new Date(2015, 0, 22, 00, 05, 0).getTime();
+  onTimeDeadline = new Date(2015, 0, 23, 12, 35, 0).getTime();
+} else {
+  earlyDeadline = new Date(2015, 0, 20, 00, 05, 0).getTime();
+  onTimeDeadline = new Date(2015, 0, 23, 12, 35, 0).getTime();
+}
 
 //filter for a specific problem
 for (var i=0; i<data.length; i++) {
-  if (data[i].exercise === 9) {
+  if (data[i].exercise === _id) {
   //if (data[i].exercise !== 0) {
     ds.push(data[i]);
   }
@@ -26,26 +40,102 @@ for (var i=0; i<ds.length; i++) {
   unique += 1;
 }
 
+
+//scales
+var xScale = d3.scale.linear()
+.domain([
+  d3.min(ds, function(d) { return d.timeMil; }),
+  d3.max(ds, function(d) { return d.timeMil; })
+])
+.range([p, w - p]);
+
+var yScale = d3.scale.linear()
+.domain([0, d3.max(ds, function(d) { return usernames[d.username]; })])
+.range([h - p, p]);
+
+
 // add the canvas to the body of the webpage
 var svg = d3.select("body")
 .append("svg")
 .attr("width", w)
 .attr("height", h)
+.attr("class", "axis")
+
+// //axes
+// svg.append("g")
+// .call(d3.svg.axis()
+//       .scale(xScale)
+//       .orient("bottom")
+// )
+// .call(d3.svg.axis()
+// .scale(yScale)
+// .orient("left")
+// )
 
 
+//points
+svg.selectAll("circle")
+.data(ds)
+.enter()
+.append("circle")
+.attr("cx", function(d) {
+  return xScale(d.timeMil);
+})
+.attr("cy", function(d) {
+  return yScale(usernames[d.username]);
+})
+.attr("r", 3)
+.attr("fill", function(d) {
+  if(d.isPassing === true) {
+    return "green";
+  } else if (d.willPass === true) {
+    return "lightblue";
+  } else {
+    return "red";
+  }
+})
+
+svg.append("line")
+.attr("stroke-width", 1)
+.attr("stroke", "black")
+.attr("x1", xScale(earlyDeadline))
+.attr("x2", xScale(earlyDeadline))
+.attr("y1", yScale(0))
+.attr("y2", yScale( d3.max( ds, function(d) { return usernames[d.username]; } ) ));
+
+svg.append("line")
+.attr("stroke-width", 1)
+.attr("stroke", "black")
+.attr("x1", xScale(onTimeDeadline))
+.attr("x2", xScale(onTimeDeadline))
+.attr("y1", yScale(0))
+.attr("y2", yScale( d3.max( ds, function(d) { return usernames[d.username]; } ) ));
 
 
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// //axes
+// var xAxis = d3.svg.axis()
+// .scale(xScale)
+// .orient("bottom")
+// .ticks(10);
+//
+// svg.append("g")
+// .attr("class", "axis")
+// .attr("transform", "translate(" + p + ",0)")
+// .call(xAxis);
 
-//scales
-var xScale = d3.scale.linear()
-.domain([
-  d3.min(dset, function(d) { return d.timeMil; }),
-  d3.max(dset, function(d) { return d.timeMil; })
-  ])
-  .range([0, w]);
-
-  var yScale = d3.scale.linear()
-  .domain([0, d3.max(dset, function(d) { return usernames[d.username]; })])
-  .range([0, h]);
+// svg.selectAll("text")
+// .data(ds)
+// .enter()
+// .append("text")
+// .text(function(d) {
+//   return d.username;
+// })
+// .attr("x", function(d) {
+//   return xScale(d.timeMil);
+// })
+// .attr("y", function(d) {
+//   return yScale(usernames[d.username]);
+// })
+// .attr("font-family", "sans-serif")
+// .attr("font-size", "11px")
+// .attr("fill", "red");
